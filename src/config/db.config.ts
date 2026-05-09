a0cf9941d19909380da3d02972ab5089c5448326
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-05-10 01:42:26 +0530

    .

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,4 +4,4 @@ import {PrismaClient} from "../../generated/prisma/client"
 const connectionString: string = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({connectionString})
-export const prisma = new PrismaClient({adapter})
+export const Prisma = new PrismaClient({adapter})
